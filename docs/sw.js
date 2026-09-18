ca328ac8a2b7fe79d68edf48bbc90b738657b875
germikpik-dotcom <germikpik@gmail.com>
2026-09-19 00:35:28 +0300

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,17 @@
+const CACHE = "fedya-vpn-v1";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
